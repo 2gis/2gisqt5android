77455e447b095d695ab226e62e6253a4f43a9289
Галин Сергей Александрович <sergey.galin@gmail.com>
2014-07-01 18:09:26 +0700

    [VNA-222] qconfig-local.h, начальная фаза

---
+++ qtbase/src/corelib/global/qconfig-local.h
@@ -0,0 +1,157 @@
+/* Dialogs */
+#ifndef QT_NO_PRINTDIALOG
+#  define QT_NO_PRINTDIALOG
+#endif
+#ifndef QT_NO_PRINTPREVIEWDIALOG
+#  define QT_NO_PRINTPREVIEWDIALOG
+#endif
+#ifndef QT_NO_WIZARD
+#  define QT_NO_WIZARD
+#endif
+
+/* Images */
+#ifndef QT_NO_IMAGEFORMAT_BMP
+#  define QT_NO_IMAGEFORMAT_BMP
+#endif
+#ifndef QT_NO_IMAGEFORMAT_PPM
+#  define QT_NO_IMAGEFORMAT_PPM
+#endif
+#ifndef QT_NO_IMAGEFORMAT_XBM
+#  define QT_NO_IMAGEFORMAT_XBM
+#endif
+#ifndef QT_NO_IMAGE_HEURISTIC_MASK
+#  define QT_NO_IMAGE_HEURISTIC_MASK
+#endif
+#ifndef QT_NO_MOVIE
+#  define QT_NO_MOVIE
+#endif
+
+/* Internationalization */
+#ifndef QT_NO_BIG_CODECS
+#  define QT_NO_BIG_CODECS
+#endif
+#ifndef QT_NO_CODECS
+#  define QT_NO_CODECS
+#endif
+
+/* ItemViews */
+#ifndef QT_NO_DATAWIDGETMAPPER
+#  define QT_NO_DATAWIDGETMAPPER
+#endif
+#ifndef QT_NO_COLUMNVIEW
+#  define QT_NO_COLUMNVIEW
+#endif
+
+/* Kernel */
+#ifndef QT_NO_EFFECTS
+#  define QT_NO_EFFECTS
+#endif
+#ifndef QT_NO_SESSIONMANAGER
+#  define QT_NO_SESSIONMANAGER
+#endif
+#ifndef QT_NO_SHAREDMEMORY
+#  define QT_NO_SHAREDMEMORY
+#endif
+
+/* Networking */
+#ifndef QT_NO_NETWORKPROXY
+#  define QT_NO_NETWORKPROXY
+#endif
+#ifndef QT_NO_SOCKS5
+#  define QT_NO_SOCKS5
+#endif
+#ifndef QT_NO_FTP
+#  define QT_NO_FTP
+#endif
+
+/* Painting */
+#ifndef QT_NO_COLORNAMES
+#  define QT_NO_COLORNAMES
+#endif
+#ifndef QT_NO_PAINT_DEBUG
+#  define QT_NO_PAINT_DEBUG
+#endif
+#ifndef QT_NO_PRINTER
+#  define QT_NO_PRINTER
+#endif
+#ifndef QT_NO_CUPS
+#  define QT_NO_CUPS
+#endif
+
+/* Styles */
+#ifndef QT_NO_STYLE_FUSION
+#  define QT_NO_STYLE_FUSION
+#endif
+#ifndef QT_NO_STYLE_STYLESHEET
+#  define QT_NO_STYLE_STYLESHEET
+#endif
+#ifndef QT_NO_STYLE_WINDOWSCE
+#  define QT_NO_STYLE_WINDOWSCE
+#endif
+#ifndef QT_NO_STYLE_WINDOWSMOBILE
+#  define QT_NO_STYLE_WINDOWSMOBILE
+#endif
+#ifndef QT_NO_STYLE_WINDOWSVISTA
+#  define QT_NO_STYLE_WINDOWSVISTA
+#endif
+#ifndef QT_NO_STYLE_WINDOWSXP
+#  define QT_NO_STYLE_WINDOWSXP
+#endif
+
+/* Utilities */
+#ifndef QT_NO_UNDOCOMMAND
+#  define QT_NO_UNDOCOMMAND
+#endif
+#ifndef QT_NO_UNDOGROUP
+#  define QT_NO_UNDOGROUP
+#endif
+#ifndef QT_NO_UNDOSTACK
+#  define QT_NO_UNDOSTACK
+#endif
+#ifndef QT_NO_UNDOVIEW
+#  define QT_NO_UNDOVIEW
+#endif
+
+/* Widgets */
+#ifndef QT_NO_LCDNUMBER
+#  define QT_NO_LCDNUMBER
+#endif
+#ifndef QT_NO_FONTCOMBOBOX
+#  define QT_NO_FONTCOMBOBOX
+#endif
+#ifndef QT_NO_CALENDARWIDGET
+#  define QT_NO_CALENDARWIDGET
+#endif
+#ifndef QT_NO_DATETIMEEDIT
+#  define QT_NO_DATETIMEEDIT
+#endif
+#ifndef QT_NO_LISTWIDGET
+#  define QT_NO_LISTWIDGET
+#endif
+#ifndef QT_NO_DOCKWIDGET
+#  define QT_NO_DOCKWIDGET
+#endif
+#ifndef QT_NO_PROGRESSBAR
+#  define QT_NO_PROGRESSBAR
+#endif
+#ifndef QT_NO_SIZEGRIP
+#  define QT_NO_SIZEGRIP
+#endif
+#ifndef QT_NO_PRINTPREVIEWWIDGET
+#  define QT_NO_PRINTPREVIEWWIDGET
+#endif
+#ifndef QT_NO_SYNTAXHIGHLIGHTER
+#  define QT_NO_SYNTAXHIGHLIGHTER
+#endif
+#ifndef QT_NO_TABLEWIDGET
+#  define QT_NO_TABLEWIDGET
+#endif
+#ifndef QT_NO_TOOLBOX
+#  define QT_NO_TOOLBOX
+#endif
+#ifndef QT_NO_WHATSTHIS
+#  define QT_NO_WHATSTHIS
+#endif
+#ifndef QT_NO_TREEWIDGET
+#  define QT_NO_TREEWIDGET
+#endif
