0ef128f4508ff3ffbdc46987dde7995eeb3f237f
Галин Сергей Александрович <sergey.galin@gmail.com>
2014-07-08 19:50:14 +0700

    [VNA-222] Собирабельный набор опций в qconfig-local

--- qtbase/src/corelib/global/qconfig-local.h
+++ qtbase/src/corelib/global/qconfig-local.h
@@ -1,14 +1,3 @@
-/* Dialogs */
-#ifndef QT_NO_PRINTDIALOG
-#  define QT_NO_PRINTDIALOG
-#endif
-#ifndef QT_NO_PRINTPREVIEWDIALOG
-#  define QT_NO_PRINTPREVIEWDIALOG
-#endif
-#ifndef QT_NO_WIZARD
-#  define QT_NO_WIZARD
-#endif
-
 /* Images */
 #ifndef QT_NO_IMAGEFORMAT_BMP
 #  define QT_NO_IMAGEFORMAT_BMP
@@ -18,9 +7,6 @@
 #endif
 #ifndef QT_NO_IMAGEFORMAT_XBM
 #  define QT_NO_IMAGEFORMAT_XBM
-#endif
-#ifndef QT_NO_IMAGE_HEURISTIC_MASK
-#  define QT_NO_IMAGE_HEURISTIC_MASK
 #endif
 #ifndef QT_NO_MOVIE
 #  define QT_NO_MOVIE
@@ -32,14 +18,6 @@
 #endif
 #ifndef QT_NO_CODECS
 #  define QT_NO_CODECS
-#endif
-
-/* ItemViews */
-#ifndef QT_NO_DATAWIDGETMAPPER
-#  define QT_NO_DATAWIDGETMAPPER
-#endif
-#ifndef QT_NO_COLUMNVIEW
-#  define QT_NO_COLUMNVIEW
 #endif
 
 /* Kernel */
@@ -54,9 +32,6 @@
 #endif
 
 /* Networking */
-#ifndef QT_NO_NETWORKPROXY
-#  define QT_NO_NETWORKPROXY
-#endif
 #ifndef QT_NO_SOCKS5
 #  define QT_NO_SOCKS5
 #endif
@@ -64,26 +39,9 @@
 #  define QT_NO_FTP
 #endif
 
-/* Painting */
-#ifndef QT_NO_COLORNAMES
-#  define QT_NO_COLORNAMES
-#endif
-#ifndef QT_NO_PAINT_DEBUG
-#  define QT_NO_PAINT_DEBUG
-#endif
-#ifndef QT_NO_PRINTER
-#  define QT_NO_PRINTER
-#endif
-#ifndef QT_NO_CUPS
-#  define QT_NO_CUPS
-#endif
-
 /* Styles */
 #ifndef QT_NO_STYLE_FUSION
 #  define QT_NO_STYLE_FUSION
-#endif
-#ifndef QT_NO_STYLE_STYLESHEET
-#  define QT_NO_STYLE_STYLESHEET
 #endif
 #ifndef QT_NO_STYLE_WINDOWSCE
 #  define QT_NO_STYLE_WINDOWSCE
@@ -96,62 +54,4 @@
 #endif
 #ifndef QT_NO_STYLE_WINDOWSXP
 #  define QT_NO_STYLE_WINDOWSXP
-#endif
-
-/* Utilities */
-#ifndef QT_NO_UNDOCOMMAND
-#  define QT_NO_UNDOCOMMAND
-#endif
-#ifndef QT_NO_UNDOGROUP
-#  define QT_NO_UNDOGROUP
-#endif
-#ifndef QT_NO_UNDOSTACK
-#  define QT_NO_UNDOSTACK
-#endif
-#ifndef QT_NO_UNDOVIEW
-#  define QT_NO_UNDOVIEW
-#endif
-
-/* Widgets */
-#ifndef QT_NO_LCDNUMBER
-#  define QT_NO_LCDNUMBER
-#endif
-#ifndef QT_NO_FONTCOMBOBOX
-#  define QT_NO_FONTCOMBOBOX
-#endif
-#ifndef QT_NO_CALENDARWIDGET
-#  define QT_NO_CALENDARWIDGET
-#endif
-#ifndef QT_NO_DATETIMEEDIT
-#  define QT_NO_DATETIMEEDIT
-#endif
-#ifndef QT_NO_LISTWIDGET
-#  define QT_NO_LISTWIDGET
-#endif
-#ifndef QT_NO_DOCKWIDGET
-#  define QT_NO_DOCKWIDGET
-#endif
-#ifndef QT_NO_PROGRESSBAR
-#  define QT_NO_PROGRESSBAR
-#endif
-#ifndef QT_NO_SIZEGRIP
-#  define QT_NO_SIZEGRIP
-#endif
-#ifndef QT_NO_PRINTPREVIEWWIDGET
-#  define QT_NO_PRINTPREVIEWWIDGET
-#endif
-#ifndef QT_NO_SYNTAXHIGHLIGHTER
-#  define QT_NO_SYNTAXHIGHLIGHTER
-#endif
-#ifndef QT_NO_TABLEWIDGET
-#  define QT_NO_TABLEWIDGET
-#endif
-#ifndef QT_NO_TOOLBOX
-#  define QT_NO_TOOLBOX
-#endif
-#ifndef QT_NO_WHATSTHIS
-#  define QT_NO_WHATSTHIS
-#endif
-#ifndef QT_NO_TREEWIDGET
-#  define QT_NO_TREEWIDGET
 #endif
